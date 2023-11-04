206f964db29cae9f6a2cb03c4cfcd1512f2973c0
jiheunkim <ziheun54@gmail.com>
2023-11-05 03:18:12 +0900

    로그인 desigin 수정 및 api test code 추가

--- frontend/src/components/pages/SignUp.js
+++ frontend/src/components/pages/SignUp.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { auth } from "../../firebase-config";
-// import { signInWithEmailAndPassword } from "firebase/auth";
+import axios from 'axios';
 import '../../App.css';
+import '../Button.css';
 
 
 const SignUp = () => {
@@ -25,14 +25,19 @@ const SignUp = () => {
     e.preventDefault();
 
     try {
-      // Firebase Authentication을 사용하여 이메일과 비밀번호로 로그인
-    //   if (email.trim() !== '' && password.trim() !== '') {
-    //     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    //     const user = userCredential.user;
-    //     setUserData(user);
-    //     console.log(user);
-    //     navigate('/');
-    //   }
+      if (email.trim() !== '' && password.trim() !== '') {
+        const response = await axios.post('/login', {
+          username: email,
+          password: password,
+        });
+
+        if (response.status === 200) {
+          navigate('/');
+          console.log('login success', response.data);
+        } else {
+          console.error('login fail');
+        }
+      }
     } catch (error) {
       console.error(error);
     }
@@ -41,83 +46,70 @@ const SignUp = () => {
 
   return (
     <>
-    <div className='App-back'>
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              로그인<br></br>
-            </h2>
+            <img
+              className="mx-auto w-1/2"
+              alt='logo_p'
+              src="/image/logo_p.png"
+            />
           </div>
-
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form className="space-y-3" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  이메일
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    value={email}
-                    required
-                    onChange={handleEmailChange} // 입력값이 변경될 때 상태 업데이트
-                    className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder='이메일'
+                  value={email}
+                  required
+                  onChange={handleEmailChange} // 입력값이 변경될 때 상태 업데이트
+                  className="block w-full rounded-md border-0 px-5 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
-
               <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    비밀번호
-                  </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder='비밀번호'
+                  value={password}
+                  required
+                  onChange={handlePasswordChange} // 입력값이 변경될 때 상태 업데이트
+                  className="block w-full rounded-md border-0 px-5 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                <div className="flex mt-3 justify-end">
                   <div className="text-sm">
-                    <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
+                    <a href="#" className="font-semibold text-green-500 hover:text-green-400">
                       비밀번호 찾기
                     </a>
                   </div>
                 </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    value={password}
-                    required
-                    onChange={handlePasswordChange} // 입력값이 변경될 때 상태 업데이트
-                    className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
               </div>
-
               <div>
                 <button
                   type="submit"
-                  className="relate w-full justify-center rounded-md bg-black py-1 text-m font-semibold leading-6 text-gray-200 shadow-sm hover:text-white hover:font-bold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 mb-2"
+                  className="relate button-style"
                 >
-                  <img className='login-google' src='https://t1.daumcdn.net/cfile/tistory/251C7B4A52BEB7A526' />
                   로그인
-                  <img className='login-google' src='https://t1.daumcdn.net/cfile/tistory/251C7B4A52BEB7A526' />
                 </button>
               </div>
             </form>
 
             <p className="mt-10 text-center text-sm text-gray-500">
-              아직 회원이 아니신가요?{'   '}
-              <a href="/register" className="font-semibold leading-6 text-blue-600 hover:text-blue-500">
+              계정이 없으신가요?{'   '}
+              <a href="/register" className="font-semibold leading-6 text-green-500 hover:text-green-400">
                 &nbsp;회원가입하기
               </a>
             </p>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </>
   );

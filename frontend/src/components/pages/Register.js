0d02e539b1f766b8f53735761cf4fd44f6670f56
jiheunkim <ziheun54@gmail.com>
2023-11-05 03:57:22 +0900

    회원가입 디자인 수정 및 api 테스트 코드 추가

--- frontend/src/components/pages/Register.js
+++ frontend/src/components/pages/Register.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-// import { auth, signOut } from "../../firebase-config";
-// import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../../App.css';
+import '../Button.css';
 
 
 const Register = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [checkedPassword, setCheckedPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
@@ -25,11 +24,11 @@ const Register = () => {
   };
 
   const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
+    setCheckedPassword(e.target.value);
   };
 
   const handleNameChange = (e) => {
-    setName(e.target.value);
+    setNickname(e.target.value);
   };
 
   // 에러 메시지 컴포넌트
@@ -43,100 +42,114 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
+    if (password !== checkedPassword) {
         setErrorMessage('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
         return;
       } else {
         setErrorMessage('비밀번호가 일치합니다.');
       }
 
-    // try {
-    //   // Firebase Authentication을 사용하여 사용자 생성
-    //   const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    //   const user = userCredential.user;
-    //   const uid = user.uid; // Firebase에서 생성된 UID
+    try {
+      // 회원가입 데이터 생성
+      const userData = {
+        email: email,
+        password: password,
+        nickname: nickname,
+        checkedPassword: checkedPassword,
+      };
 
-    //   // 사용자 이름 설정
-    //   await updateProfile(userCredential.user, {
-    //     displayName: name,
-    //   });
+      // 회원가입 요청 보내기
+      const response = await axios.post('http://localhost:8080/api/members/signup', userData);
 
-    //   // 로그아웃 처리
-    // await signOut(auth);
-    // navigate('/sign-up');
-
-    //   // 회원가입 성공 시 리다이렉트 또는 다른 작업 수행
-    //   const postData = {
-    //     fields: {
-    //       id: uid, // Firebase에서 생성된 UID
-    //       name: name, // 사용자가 입력한 이름
-    //     }
-    //   };
-
-    //   await axios.post(`/signup`, postData)
-    //   .then(response => {
-    //     // POST 요청이 성공한 경우의 처리
-    //     console.log(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.error('Error fetching data:', error);
-    //   });
-
-    // } catch (error) {
-    //   console.error('회원가입 실패:', error);
-    // }
+      // 회원가입 성공 시 리다이렉트 또는 다른 작업 수행
+      if (response.status === 200) {
+        console.log('회원가입 성공', response.data);
+        navigate('/sign-in'); // 회원가입 성공 시 로그인 페이지로 리다이렉트
+      } else {
+        console.error('회원가입 실패');
+      }
+    } catch (error) {
+      console.error('회원가입 실패:', error);
+    }
   };
 
   return (
     <>
+    <div className='main-font'>
     <section className="bg-gray-200 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                회원가입
-              </h1>
-              <form className="space-y-4 md:space-y-5" onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
-                  <input type="email" name="email" id="email" value={email} onChange={handleEmailChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required="" />
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="register-box-container w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <form className="space-y-4 md:space-y-5" onSubmit={handleSubmit}>
+              <div>
+                <input type="email" name="email" id="email" value={email} onChange={handleEmailChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="이메일" required="" />
+              </div>
+              <div>
+                <input type="text" name="nickname" id="nickname" value={nickname} onChange={handleNameChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="닉네임" required="" />
+              </div>
+              <div>
+                <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+              </div>
+              <div>
+                <input type="password" name="checkedPassword" id="checkedPassword" placeholder="비밀번호 확인" value={checkedPassword} onChange={handleConfirmPasswordChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                {/* 에러 메시지 표시 */}
+                {errorMessage && <ErrorMessage message={errorMessage} />}
+              </div>
+              <br></br>
+              <div>
+                <label htmlFor="profile" className="block mb-4 text-m font-medium text-gray-900 dark:text-white">프로필 선택</label>
+                <div className='flex justify-center ml-1 mr-1'>
+                  <img
+                    className="mx-auto w-1/5 mr-1"
+                    alt='profile_1'
+                    src="/image/profile_1.png"
+                  />
+                  <img
+                    className="mx-auto w-1/5 mr-1"
+                    alt='profile_2'
+                    src="/image/profile_2.png"
+                  />
+                  <img
+                    className="mx-auto w-1/5 mr-1"
+                    alt='profile_3'
+                    src="/image/profile_2.png"
+                  />
+                  <img
+                    className="mx-auto w-1/5 mr-1"
+                    alt='profile_4'
+                    src="/image/profile_2.png"
+                  />
+                  <img
+                    className="mx-auto w-1/5"
+                    alt='profile_5'
+                    src="/image/profile_5.png"
+                  />
                 </div>
-                <div>
-                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이름</label>
-                  <input type="text" name="name" id="name" value={name} onChange={handleNameChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="이름을 입력해주세요" required="" />
+                <br></br>
+              </div>
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                 </div>
-                <div>
-                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호</label>
-                  <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                <div className="ml-3 text-sm">
+                  <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
+                      I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                 </div>
-                <div>
-                  <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호 확인</label>
-                  <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" value={confirmPassword} onChange={handleConfirmPasswordChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-                  {/* 에러 메시지 표시 */}
-                  {errorMessage && <ErrorMessage message={errorMessage} />}
-                </div>
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-                        I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                  </div>
-                </div>
-                <button type="submit" className="w-full text-white bg-black hover:bg-black-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">회원가입하기</button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  이미 회원가입을 하셨나요?&nbsp;&nbsp;&nbsp;<a href="/sign-up" className="font-medium text-primary-600 hover:underline dark:text-primary-500">로그인하기</a>
-                </p>
-              </form>
-            </div>
+              </div>
+              <button type="submit" className="relative button-style">코깅 시작하기</button>
+              <p className="mt-10 text-center text-sm text-gray-500">
+                이미 회원가입을 하셨나요?&nbsp;&nbsp;&nbsp;<a href="/sign-up" className="font-medium text-primary-600 hover:underline dark:text-primary-500">로그인하기</a>
+              </p>
+            </form>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+    </div>
   </>
   );
 };

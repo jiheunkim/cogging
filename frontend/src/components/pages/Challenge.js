ba601187e33db0277aded2c058b8d893e9f62a3b
jiheunkim <ziheun54@gmail.com>
2023-11-10 12:22:12 +0900

    도전과제 setting

---
+++ frontend/src/components/pages/Challenge.js
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import '../../App.css';
+import '../Challenge.css';
+
+
+
+function Challenge() {
+
+  return (
+      <>
+        <div className='flex mx-auto justify-center'>
+            <p className='title-font mt-20 mb-3'>도전과제</p>
+        </div>
+        <div className='main'>
+            <div className="white-box">
+                <label htmlFor="chg1">열심히 열심히</label>
+                <div className='flex justify-start ml-10 mr-10'>
+                    <img
+                    className="chg-coin"
+                    alt='chg1'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg1'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg1'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg1'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg1'
+                    src="/image/chg_ex.png"
+                    />
+                </div>
+            </div>
+            <div className="white-box">
+                <label htmlFor="chg2">바다의 왕자</label>
+                <div className='flex justify-start ml-10 mr-10'>
+                    <img
+                    className="chg-coin"
+                    alt='chg2'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg2'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg2'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg2'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg2'
+                    src="/image/chg_ex.png"
+                    />
+                </div>
+            </div>
+            <div className="white-box">
+                <label htmlFor="chg3">산토끼 토끼야</label>
+                <div className='flex justify-start ml-10 mr-10'>
+                    <img
+                    className="chg-coin"
+                    alt='chg3'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg3'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg3'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg3'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg3'
+                    src="/image/chg_ex.png"
+                    />
+                </div>
+            </div>
+            <div className="white-box">
+                <label htmlFor="chg4">공원 수집가</label>
+                <div className='flex justify-start ml-10 mr-10'>
+                    <img
+                    className="chg-coin"
+                    alt='chg4'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg4'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg4'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg4'
+                    src="/image/chg_ex.png"
+                    />
+                    <img
+                    className="chg-coin"
+                    alt='chg4'
+                    src="/image/chg_ex.png"
+                    />
+                </div>
+            </div>
+        </div>
+        
+      </>
+
+  );
+}
+
+export default Challenge;
